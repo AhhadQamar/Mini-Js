9e592e70fcced4f0cd52cc731d3a7f24293bfe23
AhhadQamar <ahadqam1@email.com>
2026-06-25 21:45:24 +0500

    make some changes

--- quote-generator/script.js
+++ quote-generator/script.js
@@ -1,20 +1,10 @@
-
 let apiQuotes = []
-
-
-
-
-
-
 async function getQuotes() {
 const apiUrl = 'https://type.fit/api/quotes';
-
 try {
   const response = await fetch(apiUrl);
-
     apiQuotes = await response.json();
 .console.log(apiQuotes)  } catch (error) {
 }
 }
-
  getQuotes();
